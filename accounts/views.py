b3a84f055ee4e1c1098f7d043537454585f43aec
ianwanjala <ian.lukuya@strathmore.edu>
2024-03-22 02:50:04 +0300

    user activation via email implemented successfuly

--- accounts/views.py
+++ accounts/views.py
@@ -71,4 +71,17 @@ def logout (request):
     return redirect('login')
 
 def activate(request, uidb64, token):
-    return HttpResponse('ok')
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user=None
+
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active =True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect ('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
