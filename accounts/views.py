8a663022a7b09886d955fe195ed30f4d5d996daa
ianwanjala <ian.lukuya@strathmore.edu>
2024-03-25 17:54:35 +0300

    some unknown changes happened to the project, unable to access reset password page, login page from register. only those are noted, will attempt to rectify

--- accounts/views.py
+++ accounts/views.py
@@ -58,7 +58,8 @@ def login (request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
         else:
             messages.error(request,'Invalid Credentials')
             return redirect('login')
@@ -85,3 +86,38 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid activation link')
         return redirect('register')
+
+@login_required
+def dashboard (request):
+    return render (request, 'accounts/dashboard.html')
+
+@login_required
+def forgotPassword (request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user=Account.objects.get(email__exact=email)
+
+            #reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset your password'
+            message = render_to_string('accounts/reset_password_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+                })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been sent, check your email')
+            return redirect('login')
+
+        else:
+            messages.error(request, ' Account does not exist!')
+            return redirect('forgotPassword')
+    return render (request, 'accounts/forgotPassword.html')
+
+def resetpassword_validate(request):
+    return HttpResponse('okay')
