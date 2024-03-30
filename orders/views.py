28c5469fd34305360a94328d7a8ec9d82f5c5954
ianwanjala <ian.lukuya@strathmore.edu>
2024-03-30 21:23:23 +0300

    form data is not present for the required items, only item notes data is being passed to the function.

--- orders/views.py
+++ orders/views.py
@@ -26,26 +26,34 @@ def place_order(request, total=0, quantity=0,):
     tax = (2*total)/100
     grand_total=total+tax
     
-    
+    form=OrderForm() #instatiating form outside of the post request handling block
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
+            # print(form.errors)
             #storing all the billing info inside order table
             data = Order()
-            data.first_name= form.cleaned_data('first_name')
-            data.last_name= form.cleaned_data('last_name')
-            data.phone= form.cleaned_data('phone')
-            data.email= form.cleaned_data('email')
-            data.address_line_1= form.cleaned_data('address_line_1')
-            data.address_line_2= form.cleaned_data('address_line_2')
-            data.country= form.cleaned_data('country')
-            data.state= form.cleaned_data('state')
-            data.city= form.cleaned_data('city')
-            data.order_note= form.cleaned_data('order_note')
+            data.first_name= form.cleaned_data['first_name']
+            data.last_name= form.cleaned_data['last_name']
+            data.phone= form.cleaned_data['phone']
+            data.email= form.cleaned_data['email']
+            data.address_line_1= form.cleaned_data['address_line_1']
+            data.address_line_2= form.cleaned_data['address_line_2']
+            data.country= form.cleaned_data['country']
+            data.state= form.cleaned_data['state']
+            data.city= form.cleaned_data['city']
+            data.order_note= form.cleaned_data['order_note']
             data.order_total= grand_total
             data.tax= tax
             data.ip = request.META.get('REMOTE_ADDR')
             data.save()
+        else:
+            print('Invalid form data:')       
+            for field, errors in form.errors.items():
+             for error in errors:
+                print(f"{field}: {error}")
+            print('invalid form')       
+            # print(form.errors)
             return redirect( 'checkout')
 
             
