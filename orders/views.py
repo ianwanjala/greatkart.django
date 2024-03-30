4f70e3194e310973a28d69b799a15202e5d640b5
ianwanjala <ian.lukuya@strathmore.edu>
2024-03-30 23:18:31 +0300

    solved value error, problem was in html code. moving on...

--- orders/views.py
+++ orders/views.py
@@ -38,24 +38,25 @@ def place_order(request, total=0, quantity=0,):
         if form.is_valid():
             #storing all the billing info inside order table
             data = Order()
-            data.first_name= form.cleaned_data('first_name')
-            data.last_name= form.cleaned_data('last_name')
-            data.phone= form.cleaned_data('phone')
-            data.email= form.cleaned_data('email')
-            data.address_line_1= form.cleaned_data('address_line_1')
-            data.address_line_2= form.cleaned_data('address_line_2')
-            data.country= form.cleaned_data('country')
-            data.state= form.cleaned_data('state')
-            data.city= form.cleaned_data('city')
-            data.order_note= form.cleaned_data('order_note')
+            data.first_name= form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone= form.cleaned_data['phone']
+            data.email= form.cleaned_data['email']
+            data.address_line_1= form.cleaned_data['address_line_1']
+            data.address_line_2= form.cleaned_data['address_line_2']
+            data.country= form.cleaned_data['country']
+            data.state= form.cleaned_data['state']
+            data.city= form.cleaned_data['city']
+            data.order_note= form.cleaned_data['order_note']
             data.order_total= grand_total
             data.tax= tax
             data.ip = request.META.get('REMOTE_ADDR')
-            form.save()
+            data.save()
         else:
             for field, errors in form.errors.items():
-             for error in errors:
-                print(f"{field}: {error}")
+            #  for error in errors:
+                # print(f"{field}: {error}")
+                print(f"{field}: {errors}")
             # print(form.errors)
             # return redirect( 'checkout')
 
