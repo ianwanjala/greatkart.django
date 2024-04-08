d93235cdf9ef2cdaf64666cd18f5506477d0cd10
ianwanjala <ian.lukuya@strathmore.edu>
2024-04-08 21:18:05 +0300

    solved email verification error on registration i.e template not found

--- accounts/views.py
+++ accounts/views.py
@@ -35,7 +35,7 @@ def register(request):
             #user activation function
             current_site = get_current_site(request)
             mail_subject = 'Please activate your account'
-            message = render_to_string('/accounts/account_verification_email.html',{
+            message = render_to_string('accounts/account_verification_email.html',{
                 'user':user,
                 'domain':current_site,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
@@ -44,15 +44,15 @@ def register(request):
             to_email = email
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
-            # messages.success(request, 'Thank you for registration, Please check you email for activation link.')
-            return redirect('accounts/login/?command=verification&email='+email)
+            messages.success(request, 'Thank you for registration, Please check you email for activation link.')
+            #return redirect('accounts/login/?command=verification&email='+email)
 
     else:
          form = RegistrationForm()
     context = {
         'form' : form,
     }
-    return render (request, 'accounts/register.html', context )
+    return render (request, 'accounts/login.html', context )
 
 def login (request):
     if request.method == 'POST':
