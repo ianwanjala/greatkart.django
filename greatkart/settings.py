5de2004dc3fda21657390ea6a70510c6ec1663a9
Ian  Wanjala <34273325+ianwanjala@users.noreply.github.com>
2025-03-01 23:09:54 +0300

    Update settings.py
    misspelled dir folder

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'greatkart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
